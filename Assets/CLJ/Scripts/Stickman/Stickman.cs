4c9cb29486f7eac724ea359a85d6906d3d422a53
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-12-06 20:49:07 +0300

    feat: Stickman selection outline.

--- Assets/CLJ/Scripts/Stickman/Stickman.cs
+++ Assets/CLJ/Scripts/Stickman/Stickman.cs
@@ -5,9 +5,11 @@ namespace CLJ.Scripts
     public class Stickman : MonoBehaviour
     {
         [SerializeField] private StickmanAnimation _stickmanAnimation;
+        [SerializeField] private Outline _outline;
         
         public void SetSelected()
         {
+            _outline.enabled = !_outline.enabled;
             _stickmanAnimation.PlaySelectedAnimation();
         }
     }
