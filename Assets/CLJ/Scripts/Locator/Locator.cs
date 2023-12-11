087fb1ab77df985f64d16d9e81263c7cd6ab4797
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-12-11 18:30:41 +0300

    feat: Service Locator implemented.

---
+++ Assets/CLJ/Scripts/Locator/Locator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CLJ.Scripts.Locator
+{
+    public class Locator
+    {
+        static          Locator                  _instance;
+        static readonly object                   SyncRoot  = new();
+        static readonly Dictionary<Type, object> Managers = new();
+
+        public static Locator Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+
+                lock (SyncRoot)
+                {
+                    _instance ??= new Locator();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void Register<T>(object manager)
+        {
+            lock (SyncRoot)
+            {
+                if (Managers.ContainsKey(typeof(T)))
+                {
+                    Managers[typeof(T)] = manager;
+                }
+                else
+                {
+                    Managers.Add(typeof(T), manager);
+                }
+            }
+        }
+
+        public T Resolve<T>()
+        {
+            lock (SyncRoot)
+            {
+                if (!Managers.TryGetValue(typeof(T), out object manager))
+                {
+                    Debug.LogError($"Manager not found {(typeof(T).Name)}");
+                }
+
+                return (T)manager;
+            }
+        }
+
+        public void Reset()
+        {
+            lock (SyncRoot)
+            {
+                Managers.Clear();
+            }
+        }
+    }
+}
