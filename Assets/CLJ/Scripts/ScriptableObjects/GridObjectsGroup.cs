ead9e0f3f43db6abafa02aaae8eb69e1c5a43bff
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-12-11 11:52:37 +0300

    feat: Default Grid Objects Group created.

---
+++ Assets/CLJ/Scripts/ScriptableObjects/GridObjectsGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CLJ.Scripts.Runtime.Level;
+using UnityEngine;
+
+namespace CLJ.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Grid Objects Group", menuName = "Create Grid Objects Group", order = 0)]
+    public class GridObjectsGroup : ScriptableObject
+    {
+        [field:SerializeField] public List<GridObject> GridObjects { get; private set; }
+    }
+}
