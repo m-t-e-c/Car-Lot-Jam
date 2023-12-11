d6635ff36308b9c51259a657be15b13ae526c4d6
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-12-11 19:04:08 +0300

    feat: ViewManager implemented.

---
+++ Assets/CLJ/Scripts/Runtime/Managers/ViewManager/IViewManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace CLJ.Managers.ViewManager
+{
+    public interface IViewManager
+    {
+        public event Action<Component> OnViewLoaded;
+		
+        T GetView<T>() where T : Component;
+		
+        void LoadView<T>(Action<T> onLoaded = null) where T : Component;
+
+        void CloseView<T>() where T : Component;
+    }
+}
---
+++ Assets/CLJ/Scripts/Runtime/Managers/ViewManager/ViewManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Object = UnityEngine.Object;
+
+namespace CLJ.Managers.ViewManager
+{
+    public class ViewManager : IViewManager
+    {
+        readonly Dictionary<Type, GameObject> viewInstances = new Dictionary<Type, GameObject>();
+
+        public event Action<Component> OnViewLoaded;
+
+        public T GetView<T>() where T : Component
+        {
+            Type viewType = typeof(T);
+
+            if (viewInstances.TryGetValue(viewType, out GameObject viewObject))
+            {
+                return viewObject.GetComponent<T>();
+            }
+
+            Debug.LogError("View not found for type: " + viewType.Name);
+            return null;
+        }
+
+        public void LoadView<T>(Action<T> onLoaded = null) where T : Component
+        {
+            Type viewType = typeof(T);
+
+            if (viewInstances.TryGetValue(viewType, out GameObject viewObject))
+            {
+                viewObject.SetActive(true);
+                onLoaded?.Invoke(viewObject.GetComponent<T>());
+                return;
+            }
+
+            Addressables.LoadAssetAsync<GameObject>(viewType.Name).Completed += handle =>
+            {
+                if (handle.Status == AsyncOperationStatus.Succeeded)
+                {
+                    GameObject prefab        = handle.Result;
+                    GameObject newViewObject = Object.Instantiate(prefab);
+                    T          viewComponent = newViewObject.GetComponent<T>();
+
+                    if (viewComponent != null)
+                    {
+                        viewInstances[viewType] = newViewObject;
+                        OnViewLoaded?.Invoke(viewComponent);
+                        onLoaded?.Invoke(viewComponent);
+                    }
+                    else
+                    {
+                        Debug.LogError("View component not found on prefab for type: " + viewType.Name);
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Failed to load view prefab for type: " + viewType.Name);
+                }
+            };
+        }
+
+        public void CloseView<T>() where T : Component
+        {
+            Type viewType = typeof(T);
+
+            if (viewInstances.TryGetValue(viewType, out GameObject viewObject))
+            {
+                viewInstances.Remove(viewType);
+                Object.Destroy(viewObject);
+            }
+        }
+    }
+}
