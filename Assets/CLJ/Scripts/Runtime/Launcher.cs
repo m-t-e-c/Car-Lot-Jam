b3468a11e2dca6a6e1f4ea2f11c0ef488431a370
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-12-13 19:48:08 +0300

    feat: Camera reference added to Launcher.

--- Assets/CLJ/Scripts/Runtime/Launcher.cs
+++ Assets/CLJ/Scripts/Runtime/Launcher.cs
@@ -1,4 +1,5 @@
-﻿using CLJ.Managers.ViewManager;
+﻿using System;
+using CLJ.Managers.ViewManager;
 using CLJ.Managers.LevelManager;
 using UnityEngine;
 
@@ -6,17 +7,22 @@ namespace CLJ.Runtime
 {
     public class Launcher : MonoBehaviour
     {
+        [SerializeField] private CameraHolder cameraHolder;
+        
         private ILevelManager _levelManager;
         private IViewManager _viewManager;
+        
+        [SerializeField] private int _levelIndex = 1;
 
         private void Awake()
         {
-            _levelManager = new LevelManager();
+            _levelManager = new LevelManager(_levelIndex);
             Locator.Instance.Register<ILevelManager>(_levelManager);
             
             _viewManager = new ViewManager();
             Locator.Instance.Register<IViewManager>(_viewManager);
-
+            
+            Locator.Instance.Register<CameraHolder>(cameraHolder);
         }
     }
 }
