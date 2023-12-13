623d5088ecae3e0014f37565c37ff7a045ff976e
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-12-13 19:45:07 +0300

    feat: Camera switch between Orthographic and Perspective views based on the grid.

---
+++ Assets/CLJ/Scripts/Runtime/CameraHolder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace CLJ.Runtime
+{
+    public enum CameraType
+    {
+        Orthographic,
+        Perspective
+    }
+
+    public class CameraHolder : MonoBehaviour
+    {
+        [SerializeField] private Transform _otrhographicCameraTransform;
+        [SerializeField] private Transform _perspectiveCameraTransform;
+        [SerializeField] private float _orthographicSize;
+
+        private Camera _camera;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        public void SetCamera(CameraType cameraType)
+        {
+            var cameraTransform = _camera.transform;
+            if (cameraType.Equals(CameraType.Perspective))
+            {
+                _camera.orthographic = false;
+                cameraTransform.position = _perspectiveCameraTransform.position;
+                cameraTransform.rotation = _perspectiveCameraTransform.rotation;
+            }
+            else if (cameraType.Equals(CameraType.Orthographic))
+            {
+                _camera.orthographic = true;
+                _camera.orthographicSize = _orthographicSize;
+                cameraTransform.position = _otrhographicCameraTransform.position;
+                cameraTransform.rotation = _otrhographicCameraTransform.rotation;
+            }
+        }
+    }
+}
