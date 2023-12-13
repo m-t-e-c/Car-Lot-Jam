ee9c6a72775dbc7d15a7bc25f38b3a15db8bc8be
Emir Çelik <44502728+m-t-e-c@users.noreply.github.com>
2023-12-13 19:48:47 +0300

    feat: Moveable ground highlight system.

--- Assets/CLJ/Scripts/Runtime/Ground.cs
+++ Assets/CLJ/Scripts/Runtime/Ground.cs
@@ -1,10 +1,32 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 namespace CLJ.Runtime
 {
     public class Ground : MonoBehaviour
     {
-        Vector2Int _coordinates;
+        [SerializeField] private Renderer _renderer;
+
+        private Material _material;
+        private Vector2Int _coordinates;
+
+        private void Start()
+        {
+            _material = new Material(_renderer.sharedMaterial);
+            _renderer.sharedMaterial = _material;
+        }
+
+        public void Highlight(bool canMovable)
+        {
+            if (canMovable)
+            {
+                _renderer.sharedMaterial.DOColor(new Color(0.35f, 1f, 0.43f), 0.3f).SetLoops(2, LoopType.Yoyo);
+            }
+            else
+            {
+                _renderer.sharedMaterial.DOColor(new Color(1f, 0.28f, 0.28f), 0.3f).SetLoops(2, LoopType.Yoyo);
+            }
+        }
         
         public void SetCoordinates(int x, int y)
         {
